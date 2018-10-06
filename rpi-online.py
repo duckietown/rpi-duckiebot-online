b5eda86ae9078bb45aebc2823d6f091aebdd6ae0
Andrea Censi <acensi@ethz.ch>
2018-10-06 17:44:11 +0200

    done

--- rpi-online.py
+++ rpi-online.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python2
+import os
 
 def rpi_online():
     print('rpi_online is empty for now')
+
+    token = '/secrets/tokens/dt1'
+    if os.path.exists(token):
+        print('token is set')
+    else:
+        print('token is not set')
 
 
 if __name__ == '__main__':
